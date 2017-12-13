32300eefa562d713cd6a512e4bc8a6e9dba264ee
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2017-12-13 09:49:59 -0800

    remove blank lines, untabify, reindent

--- sketch.ino
+++ sketch.ino
@@ -1,56 +1,14 @@
 #include <Adafruit_GFX.h>
-
-
-
 #include <gfxfont.h>
-
-
-
-
-
 #include <Adafruit_ILI9341_8bit.h>
-
-
-
-
 
 Adafruit_ILI9341_8bit_STM tft = Adafruit_ILI9341_8bit_STM();
 
-
-
 void setup() {
-
-
-
   Serial.begin(115200);
-
-
-
-  Serial.println("ILI9341 Test!"); 
-
-
-
-  
-
-
-
- 
-
-
-
+  Serial.println("ILI9341 Test!");
   tft.begin();
-
-
-
-
-
-
-
   // read diagnostics (optional but can help debug problems)
-
-  
-
-
 }
 
 void loop() {
